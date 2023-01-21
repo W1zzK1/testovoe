384fc02c6ab56008a4f25df86dabe1ce95819bee
Gorbunov Vladislav <91543904+W1zzK1@users.noreply.github.com>
2023-01-21 09:48:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import os
 from pydrive.auth import GoogleAuth
 import pandas as pd
 from pydrive.drive import GoogleDrive
@@ -13,49 +12,57 @@ url = "https://confluence.hflabs.ru/pages/viewpage.action?pageId=1181220999"
 codes = []
 meaning = []
 
+file_name = 'result.xlsx'
+
 def parsingAPI():
     '''
     Парсинг самого кода ответа и описания для каждого
     '''
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
-    soup = bs(response.text, 'lxml')
+    try:
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
+        soup = bs(response.text, 'lxml')
 
-    rows = soup.find_all('td', class_="confluenceTd")
+        rows = soup.find_all('td', class_="confluenceTd")
 
-    count = 0
-    for i in rows:
-        if count % 2 == 0:
-            codes.append(i.text)
-            count += 1
-        else:
-            meaning.append(i.text)
-            count += 1
+        count = 0
+        for i in rows:
+            if count % 2 == 0:
+                codes.append(i.text)
+                count += 1
+            else:
+                meaning.append(i.text)
+                count += 1
 
-    return codes, meaning
+        if len(codes) != len(meaning):
+            raise Exception("Значения двух массивов не равны")
+
+        return codes, meaning
+    except:
+        return "Произошла ошибка при парсинге данных"
 
 def write_pandas():
     '''
-    Запись получченных данных в эксель таблицу через pandas
+    Запись полученных данных в эксель таблицу через pandas
     '''
     df = pd.DataFrame({'Code': codes,
                        'Meaning': meaning})
-    writer = pd.ExcelWriter('result.xlsx')
+    writer = pd.ExcelWriter(file_name)
     df.to_excel(writer)
     writer.save()
 
 
 def upload_dir(file_path=''):
     '''
-    Загрузка фалйа на гугл диск
+    Загрузка файла на гугл диск
     '''
     try:
         drive = GoogleDrive(gauth)
 
-        file_list = drive.ListFile({'q': "title contains 'result' and trashed=false"}).GetList()
+
+        file_list = drive.ListFile({'q': f"title contains '{file_name}' and trashed=false"}).GetList()
         print(file_list[0]['title'])
         file_id = file_list[0]['id']
 
-        file_name = 'result.xlsx'
         my_file = drive.CreateFile({'id': file_id, 'title': file_name})
         my_file.SetContentFile(file_path)
         my_file.Upload()
@@ -68,7 +75,7 @@ def upload_dir(file_path=''):
 def main():
     parsingAPI()
     write_pandas()
-    print(upload_dir(file_path='C:/Users/W1zzA/PycharmProjects/testovoe/result.xlsx'))
+    print(upload_dir(file_path=file_name))
 
 
 if __name__ =='__main__':
